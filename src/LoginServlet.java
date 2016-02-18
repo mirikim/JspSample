1f3a946411e53d16851ff466eeedf63ac1a893ca
mrk <rlaalfl92@nate.com>
2016-02-18 17:35:37 +0900

    Day-35

---
+++ src/LoginServlet.java
@@ -0,0 +1,44 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+/* /login이라는 url-pattern으로 요청이 날라오면 밑의 클래스를 실행하겠다 */
+@WebServlet("/login")
+
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Default constructor.
+	 */
+	public LoginServlet() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	//HttpServletRequest request, HttpServletResponse response =>웹컨테이너가갖고있는 built in객체? 
+	//생성할필요가없다 내장되어있음
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String id = request.getParameter("id");
+		String passwd = request.getParameter("passwd");
+		response.setContentType("text/html;charset=euc-kr");
+		PrintWriter out = response.getWriter();
+		out.println("아이디=" + id + "<br>");
+		out.println("비밀번호=" + passwd + "<br>");
+	}
+
+}
